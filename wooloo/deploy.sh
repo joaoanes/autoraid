98444b69792cfc00b2a57be7f634b03e2aecf1a5
João Anes <joao.anes@gmail.com>
2020-09-13 05:33:40 +0100

    Make deploy script inject version numbers

--- wooloo/deploy.sh
+++ wooloo/deploy.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-rm -rf ./public/raid_bosses.json
-curl "https://pogoapi.net/api/v1/raid_bosses.json" > ./public/raid_bosses.json
-yarn build
+BUMP=${1:-patch}
+
+../build_boss_registry.sh
+
+yarn version "--$BUMP"  
+REACT_APP_CYNDAQUIL_VERSION=$(cat ../mix.exs | grep -oP 'version: "\K(.*)(?=")') \
+  REACT_APP_WOOLOO_VERSION=$(cat package.json | jq -r .version) \
+  yarn build
+
 $(yarn global bin)/netlify deploy --dir ./build --prod
